442eebadd954c040dba00c538f79a343734ae81a
Noam Ross <noam.ross@gmail.com>
2016-10-13 15:09:32 -0400

    Implement parallelism and reproducibility

--- R/simulate.R
+++ R/simulate.R
@@ -5,26 +5,36 @@
 #' @param n_sims number of simulations to run
 #' @param parallel run the simulations in parallel?
 #' @param progress TRUE/FALSE show a progress bar?
-#' @importFrom dplyr rename_ mutate_ recode select_ arrange_
+#' @importFrom dplyr rename_ mutate_ recode select_ arrange_ bind_rows
 #' @importFrom tibble as_tibble
+#' @importFrom parallel mclapply
 #' @export
-mf_sim <- function(init, parameters, times, n_sims=1, seed=NULL, parallel=FALSE, progress=TRUE) {
+mf_sim <- function(init, parameters, times, n_sims=1, seed=NULL, parallel=FALSE, progress=FALSE) {
   progress = progress & !parallel  #progress bars don't work in parallel
 
   old_rng_kind = RNGkind("L'Ecuyer-CMRG")  # set a parallel-friendly RNG generator
-  on.exit(RNGkind(old_rng_kind[1], old_rng_kind[2])) # change it back when done
+  on.exit(RNGkind(old_rng_kind[1], old_rng_kind[2]), add=TRUE)
 
-  if(!is.null(seed)) set.seed(seed)
+  if(!is.null(seed)) {
+    old_seed <- .Random.seed
+    set.seed(seed)
+    on.exit(assign(".Random.seed", old_seed, envir=globalenv()), add=TRUE)
+  }
 
-  results = sim_gillespie(init=init, parmlist=parameters, times=times, progress=progress)
 
-  results = tibble::as_tibble(reshape2::melt(results))
+  if(parallel) n_cores = getOption("mc.cores", 2L) else n_cores = 1
+
+  results = mclapply(seq_len(n_sims), function(sim) {
+    reshape2::melt(sim_gillespie(init=init, parmlist=parameters, times=times, progress=progress))
+  }, mc.set.seed=TRUE, mc.silent = TRUE)
+
+  results = as_tibble(bind_rows(results, .id = "sim"))
   results = rename_(results, .dots = c("patch"="Var1", "class"="Var2", "time"="Var3", "population"="value"))
-  results = mutate_(results, class = ~as.character(class))
+#  results = mutate_(results, class = ~as.character(class))
   results = mutate_(results, class =  ~recode(class, `1`="S", `2`="I", `3`="R", `4`="V"))
   results = mutate_(results, class =  ~factor(class, levels=(c("S", "I", "R", "V"))))
-  results = select_(results, "time", "patch", "class", "population")
-  results = arrange_(results, "time", "patch", "class")
+  results = select_(results, "sim", "time", "patch", "class", "population")
+  results = arrange_(results, "sim", "time", "patch", "class")
   return(results)
 }
 
