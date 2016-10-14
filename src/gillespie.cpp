a260f1739c129b9a616ec2675f9928ecb9c11ee3
Noam Ross <noam.ross@gmail.com>
2016-10-14 15:35:16 -0400

    interim core simulator updates

--- src/gillespie.cpp
+++ src/gillespie.cpp
@@ -12,7 +12,6 @@ arma::cube sim_gillespie(const arma::mat &init, const List parmlist, const arma:
   mf_parmlist parms(parmlist);
   arma::mat state = init;
   arma::cube results = zeros<arma::cube>(init.n_rows, init.n_cols, times.n_elem);
-
   double time = times(0);
   double time_next;
   arma::vec::const_iterator next_record_time = times.begin();
@@ -26,16 +25,18 @@ arma::cube sim_gillespie(const arma::mat &init, const List parmlist, const arma:
  // double randnum;
 //  arma::vec vvec;
   results.slice(record) = state;
-
   Progress p(times.n_elem, progress);
   while(*(next_record_time) < time_max) {
+
     update_rates(rates, state, parms);
     double sum_rates = accu(rates);
     if(sum_rates == 0) {
       time_next = time_max + 1;
     } else {
-      time_next = time + R::rexp(sum_rates);
+      time_next = time + R::rexp(1/sum_rates);
     }
+//    Rcout << time << "; " << time_next  << "; " << *(next_record_time) << "; " << time_max  << "; " << sum_rates << std::endl;
+//    Rcout << rates << std::endl;
 //    Rcout << sum_rates << ";  " << time << "; " << time_next << std::endl;
       while(time_next > *(next_record_time) && *(next_record_time) < time_max) {  //only store times on the grid
       Progress::check_abort();
