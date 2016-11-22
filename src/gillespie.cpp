49546943e2c0afc10197204ba751738485c7db47
Noam Ross <ross@ecohealthalliance.org>
2016-11-22 15:18:36 -0500

    Speedup core function by avoiding repeat summing

--- src/gillespie.cpp
+++ src/gillespie.cpp
@@ -13,9 +13,10 @@ arma::mat sim_gillespie(const arma::vec &init, const List parmlist, const arma::
   arma::vec state = init;
   arma::vec rates = set_rates(state, parms, vals);
 
-  arma::vec cumrates = zeros<arma::vec>(rates.n_elem);
-
+  arma::vec cumrates = cumsum(rates);
+  double sum_rates = cumrates(cumrates.n_elem - 1);
   arma::mat results = zeros<arma::mat>(init.n_elem, times.n_elem);
+
   double time = times(0);
   double time_next;
   arma::vec::const_iterator next_record_time = times.begin();
@@ -29,7 +30,6 @@ arma::mat sim_gillespie(const arma::vec &init, const List parmlist, const arma::
   Progress p(times.n_elem, progress);
 
   while(*(next_record_time) < time_max) {
-    double sum_rates = accu(rates);
     if(sum_rates == 0) {
       time_next = time_max + 1;
     } else {
@@ -46,11 +46,13 @@ arma::mat sim_gillespie(const arma::vec &init, const List parmlist, const arma::
 
     if(sum_rates == 0) break;
 
-    cumrates = cumsum(rates) / sum_rates;
-    event = as_scalar(find(cumrates >= R::runif(0,1), 1));
+    event = as_scalar(find(cumrates >= R::runif(0, sum_rates), 1, "first"));
 
     update_state(state, parms, vals, event);
     update_rates(rates, state, parms, vals, event);
+    cumrates = cumsum(rates);
+    sum_rates = cumrates(cumrates.n_elem - 1);
+
     //Rcout << time << "; " << event << "; " << vals.action << "; " << vals.patch << "; " << cumrates.t();
     //Rcout << state.t();
 
