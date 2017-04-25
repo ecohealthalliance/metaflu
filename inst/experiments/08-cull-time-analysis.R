da097b1ebaadb3c58b35d25383f6334a2bb4c6aa
Kate Royce <katherine.p.royce.19@dartmouth.edu>
2017-04-25 11:58:47 -0400

    Preliminary results from varying t-detect

--- inst/experiments/08-cull-time-analysis.R
+++ inst/experiments/08-cull-time-analysis.R
@@ -34,8 +34,8 @@ print(farm_number)
     lambda = 0,     #force of infection from external sources
     tau_crit = 5,   #critical suveillance time
     I_crit = 1,     #threshold for reporting
-    pi_report = 0.9, #reporting probability
-    pi_detect = 0.9, #detection probability
+    pi_report = 1, #reporting probability
+    pi_detect = 1, #detection probability (1 for cleanliness)
     cull_time = 1,   #time to detect, which will be changed in this simulation
     network_type = "smallworld",
     network_parms = list(dim = 1, size = farm_number, nei = 2.33, p = 0.0596, multiple = FALSE, loops = FALSE),
@@ -43,12 +43,12 @@ print(farm_number)
     )
 
 
-cull_time_vector <- c(7, 365, 1095, 1825, 2555, 3285, 3650) #1 week; 1, 3, 5, 7, 9, 10 years
+cull_time_vector <- c(1,5,10,20,30,40,50)
 
 results_list <- lapply(cull_time_vector, function(x){
   print(x)
   parms["cull_time"] <- x
-  sims <- 1000
+  sims <- 100
   g_list <- mclapply(seq_len(sims), function(y){
     patches <- grow_patches_clustered(basic_patches(40,100))
     i_patches <- seed_initial_infection(patches)
@@ -58,22 +58,81 @@ results_list <- lapply(cull_time_vector, function(x){
   return(do.call("abind", g_list))
 })
 
-saveRDS(results_list, "cull_time.rds")
+saveRDS(results_list, "cull_time.rds", compress = FALSE)
+#delete before committing, as this is a lot of space
 
-print("graphing infections")
+#print("graphing infections")
 tot_i_list <- lapply(results_list, function(x) get_tot_infections_array(x))
 inf_means <- sapply(tot_i_list, function(x) mean(x$total_i))
 infection_df <- data.frame(cull_time = cull_time_vector, mean_infections = inf_means)
 ggplot(data = infection_df) + geom_point(aes(x = cull_time, y = mean_infections)) +
   labs(x = "days to culling", y = "mean number of infections")
 
-print("graphing duration")
+#more simulations
+#plot graphs like Cale's for 1 run
+#on a log scale--culling vs infections (this will help with extension too (40-50 days))
+#output: fraction of successful outbreaks
+#Sample graphs (like Cale's) aggressive vs nonaggressive culling
+
+#print("graphing duration")
 duration_list <- lapply(results_list, function(x) get_duration_array(x))
 dmeans <- sapply(duration_list, function(x) mean(x$duration))
 duration_df <- data.frame(cull_time = cull_time_vector, mean_durations = dmeans)
 ggplot(data = duration_df) + geom_point(aes(x = cull_time, y = mean_durations)) +
   labs(x = "days to culling", y = "duration of epidemic")
 
+#get oubreak proportions
+farm_num <- lapply(results_list, function(x) get_number_farms(x))
 
+outbreak_list <- unlist(lapply(farm_num, function(x){
+  return(sum(x > 1)/length(x))
+}))
 
+#make nice graphs
 
+create_graph_panel <- function(result_array, value){
+
+  gdurations <- get_duration_array(result_array)
+
+  gs.df <- get_all_sims("S", result_array)
+  gi.df <- get_all_sims("I", result_array)
+  gr.df <- get_all_sims("R", result_array)
+
+  gfarm_no <- get_number_farms(result_array)
+
+  gtot_i <- get_tot_infections_array(result_array)
+
+  gsir.df <- ggplot() +
+    geom_line(data = gs.df, aes(x = time, y = pop, group = sim), alpha = 0.05, color = "blue") +
+    geom_line(data = gi.df, aes(x = time, y = pop, group = sim), alpha = 0.05, color = "red") +
+    geom_line(data = gr.df, aes(x = time, y = pop, group = sim), alpha = 0.05, color = "green") +
+    labs(title = paste("Time to Culling:",value, "days"), x = "Time", y = "Population") +
+    scale_colour_manual(name = "Compartment", values=c(S = "blue", I = "red", R = "green")) +
+    theme_minimal()
+#plot(gsir.df)
+
+  gtotal_i <- ggplot() +
+    geom_histogram(data = gtot_i, aes(x = total_i), bins = 100) +
+    labs(title = "Infections", x = "Infections", y = "Number of Simulations") +
+    theme_minimal()
+
+  gfarm_numbers <- ggplot() +
+    geom_histogram(aes(x = gfarm_no), bins = 100) +
+    labs(title = "Farm Spread", x = "Farms Infected", y = "Number of Simulations") +
+    coord_flip() +
+    theme_minimal()
+
+  gduration <- ggplot() +
+    geom_histogram(data = gdurations, aes(x = duration), bins = 100) +
+    theme_minimal() +
+    labs(title = "Duration of Epidemic", x = "Duration in Days", y = "Number of Simulations")
+
+  lay <- rbind(c(1,1,1,4),
+               c(1,1,1,4),
+               c(2,2,3,3))
+
+  grid.arrange(gsir.df,gtotal_i,gduration,gfarm_numbers, layout_matrix = lay)
+
+}
+
+graph_panel <- purrr::map2(results_list,cull_time_vector, function(x,y) create_graph_panel(x,y))
